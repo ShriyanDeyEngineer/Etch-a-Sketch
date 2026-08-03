26fcd98ab211a95126e0dc10d3df446731789233
ShriyanDeyEngineer <shriyandey07@gmail.com>
2026-08-03 02:17:58 -0500

    Finish up project, fix last feature
    first removed unnecessary comments and code. Fixed the feature where when the user selects a grid size, the web page should display a new grid (square shape) based on the dimension selected by the user.
    
    removed the div rows and replaced it with flex wrap instead.
    
    determined the width of each unit square div by using aspect ratio and % units with the toFixed() method instead of px units to fix the issue where the grid units would form a new incomplete row and not properly fill a square div container.
    
    overall, this mini project is finished. Everything was easy except for figuring out how to manipulate the proportions within a fixed container size based on different user inputted dimensions for the unit grid.

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
 //set up the 16 x 16 grid
-//const grid = []; //something to hold each grid unit's data
 const gridContainer = document.querySelector("#grid-container");
-let numRows = 50;
-let numColumns = 50;
+const gridContainerSize = 960;
+let numGridUnits = 256;
 
 const colors = ["#0000FF", "#8A2BE2", "#7FFF00", "#B8860B", "#FF8C00", "#006400", "#8B008B", "#00FFFF", "#FFDEAD", "#808000", "#AFEEEE", "#008080","#B22222", "#483D8B", "#7FFFD4"];
 
 
 //utilize a loop, focus on the process that needs to be repeated (creating a grid unit and adding it to the grid)
-function createGrid(numRows, numColumns)
+function createGrid(numGridUnits)
 {
-    for(let i = 0; i < numRows; i++)
+    for(let i = 1; i <= numGridUnits; i++)
     {
-        const gridRow = document.createElement("div"); //create a div to store a row of divs
-        gridRow.classList.add("row");
-
-        gridContainer.appendChild(gridRow); //add the row to the container
-
-        //grid[i] = []; //create a row in the grid
-        for(let j = 0; j < numColumns; j++)
-        {
-            const gridUnit = document.createElement("div"); //create a div unit to store in a row div
-            gridUnit.classList.add("unit");
-            //gridUnit.textContent = "X";
-
-            gridRow.appendChild(gridUnit); //add the grid unit div to a row div
-            
-            //grid[i][j] = gridUnit;
-        }
+        const gridUnit = document.createElement("div"); //create a div unit to store in a row div
+        gridUnit.classList.add("unit"); //assign an identifier to the unit
+        gridUnit.style.width = String((100 / Math.sqrt(numGridUnits)).toFixed(10)) + "%";/*String(Math.floor(gridContainerSize / Math.sqrt(numGridUnits))) + "px";*/ //set the width based on the selected number of grid units
+        
+        gridContainer.appendChild(gridUnit); //add the grid unit div to a row div
     }
 }
-createGrid(numRows, numColumns);
+createGrid(numGridUnits);
 
 
 //add an event listener to all units so that when the mouse goes over them, they are colored
@@ -51,7 +39,7 @@ attatchDrawFeatureToGridUnits(allUnits);
 
 
 //event listener and loop to handle setting the grid size by the user's input
-let resizeGridButton = document.querySelector("button")
+let resizeGridButton = document.querySelector("button");
 
 resizeGridButton.addEventListener("click", event => {
     let userSelectedSize = prompt("Resize the square grid by inputting a positive integer (eg. '20' for a 20 x 20 grid). Maximum size is '100'. Minimum size is '4'.");
@@ -64,11 +52,12 @@ resizeGridButton.addEventListener("click", event => {
         userSelectedSize = Number(userSelectedSize);
     }
 
-    numRows = userSelectedSize; numColumns = userSelectedSize;
+    numGridUnits = userSelectedSize * userSelectedSize;
 
-    document.querySelectorAll(".unit").forEach(element => element.remove()); //delete current unit grid
+    //delete current unit grid by deleting each grid unit
+    document.querySelectorAll(".unit").forEach(element => element.remove());
 
     //build new unit grid based off of the user input
-    createGrid(numRows, numColumns);
+    createGrid(numGridUnits);
     attatchDrawFeatureToGridUnits(document.querySelectorAll(".unit"));
 });
